3670831cb32c3990484a843e3f76d2e8e2ee64b7
Ali Hashim <ahashim@u.rochester.edu>
2017-10-31 17:48:35 -0400

    Initial Commit
    Index file created

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+asdfasdf
+<head>
+    <meta charset="utf-8">
+    <title>Assignment 6</title>
+    
+
+     <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css">
+
+    
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+
+<body>
+Hello
+
+  
+
+
+<!-- Downloading jQuery and Materialize -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+
+
+
+</body>
+
+</html>
