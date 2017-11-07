2b055c830dc4363d4caa7abcad88cecfba35984e
JDDenzer9 <31746765+JDDenzer9@users.noreply.github.com>
2017-11-07 17:39:46 -0500

    added overrides.css

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css">
+	
+	<!--Import overrides.css-->
+    <link type="text/css" rel="stylesheet" href="css/overrides.css">
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
