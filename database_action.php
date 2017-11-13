0caa1b8525fd53b0e8486975f93e3e793a2a5ff9
Ali Hashim <ahashim@u.rochester.edu>
2017-11-13 14:51:21 -0500

    Minor changes

--- database_action.php
+++ database_action.php
@@ -11,8 +11,6 @@
     $name = Trim(stripslashes($_POST['name']));
     $email = Trim(stripslashes($_POST['email']));
     $framework = Trim(stripslashes($_POST['framework']));
-    $feature = Trim(stripslashes($_POST['feature']));
-    $improveFeat = Trim(stripslashes($_POST['improveFeat']));
     $suggestion = Trim(stripslashes($_POST['suggestion']));
     $recommend = Trim(stripslashes($_POST['recommend']));
 
@@ -20,16 +18,13 @@
     $name = mysqli_real_escape_string($connection, $name);
     $email = mysqli_real_escape_string($connection, $email);
     $framework = mysqli_real_escape_string($connection, $framework);
-    $feature = mysqli_real_escape_string($connection, $feature);
-    $improveFeat = mysqli_real_escape_string($connection, $improveFeat);
     $suggestion = mysqli_real_escape_string($connection, $suggestion);
     $recommend = mysqli_real_escape_string($connection, $recommend);
 
 
     $total = count($_POST['feature']);
     $i=0;
-    // These print each checked box, working on inputting it into database
-    if(isset($_POST['feature']))
+    if(isset($_POST['feature'])){
         foreach ($_POST['feature'] as $featureName) {
             $i++;
 
@@ -38,10 +33,11 @@
                 $feature .= ", ";
             }
         }
+    }
 
     $total = count($_POST['feature']);
     $i=0;    
-    if(isset($_POST['improveFeat']))
+    if(isset($_POST['improveFeat'])){
         foreach ($_POST['improveFeat'] as $improveFeatName) {
             $i++;
            $improveFeat .= $improveFeatName;
@@ -49,9 +45,7 @@
                 $improveFeat .= ", ";
             }
         }
-
-
-
+    }
 
     // // 2. Perform database query
     $query  = "INSERT INTO frameworkSurvey (";
